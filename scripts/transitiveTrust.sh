48fd9bbf93739f4f9e467da7b1569cf6a171cb21
Andreas Scheibal <andreas.scheibal@t-systems.com>
2023-07-06 16:24:26 +0200

    Update transitiveTrust.sh

--- scripts/transitiveTrust.sh
+++ scripts/transitiveTrust.sh
@@ -4,16 +4,19 @@ echo "Start Transitive Trust"
 
 rm -rf transit
 git clone https://$GITHUB_TOKEN@$TRANSITIVE_TRUST transit
+tree
 cd transit
-cd $ENV
+cd UAT
 mkdir signing 
 cd signing
 echo $PWD
+tree
 echo -n "${NB_UP_SIGNING_PUB}" > pub-key.pem
 echo -n "${NB_UP_SIGNING_KEY}" > priv-key.pem
 cd .. 
 cd ..
 echo $PWD
-./extract.sh $ENV $1     
+./extract.sh UAT    
 cd ..
 echo "Finished Transitive Trust"
+tree
