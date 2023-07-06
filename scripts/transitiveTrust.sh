208563a4cef8968c169ae4037d721a0ecd720b1d
Andreas Scheibal <andreas.scheibal@t-systems.com>
2023-07-06 16:09:15 +0200

    Update transitiveTrust.sh

--- scripts/transitiveTrust.sh
+++ scripts/transitiveTrust.sh
@@ -7,11 +7,13 @@ git clone https://$GITHUB_TOKEN@$TRANSITIVE_TRUST transit
 cd transit
 cd $ENV
 mkdir signing 
-cd signing     
-echo "$NB_UP_SIGNING_PUB}" > pub-key.pem
+cd signing
+echo $PWD
+echo "$NB_UP_SIGNING_PUB" > pub-key.pem
 echo "$NB_UP_SIGNING_KEY" > priv-key.pem
 cd .. 
 cd ..
+echo $PWD
 ./extract.sh $ENV $1     
 cd ..
 echo "Finished Transitive Trust"
