a1bf462b1af2ea39af44b702d11b275078d7838c
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 11:45:53 +0200

    Update transitiveTrust.sh

--- scripts/transitiveTrust.sh
+++ scripts/transitiveTrust.sh
@@ -4,18 +4,14 @@ echo "Start Transitive Trust"
 
 rm -rf transit
 git clone https://$BOT_TOKEN_GITHUB@$TRANSITIVE_TRUST_SOURCE transit
-tree
 cd transit
 cd $ENV
 mkdir signing 
 cd signing
-echo $PWD
-tree
 echo "$NB_UP_SIGNING_PUB" > pub-key.pem
 echo "$NB_UP_SIGNING_KEY" > priv-key.pem
 cd .. 
 cd ..
-echo $PWD
 ./extract.sh $ENV $1 
 cd ..
 echo "Finished Transitive Trust"
