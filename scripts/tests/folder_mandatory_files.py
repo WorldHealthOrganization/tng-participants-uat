4ca3dd9889274a8482fb7f34c835389cb7e24c4f
arajnor99 <153087310+arajnor99@users.noreply.github.com>
2026-02-05 13:44:22 +0530

    indentation correction

--- scripts/tests/folder_mandatory_files.py
+++ scripts/tests/folder_mandatory_files.py
@@ -5,15 +5,13 @@ def test_folder_mandatory_files(country_folder):
 
     ofiles = collect_onboarding_files(country_folder)
 
-
     assert len(ofiles) >= 1, 'There must be at least one domain'
 
     # Testing folder structure
     for domain in ofiles.keys():
-        if domain in allowed_domains:
-           assert domain in ('DCC','IPS-PILGRIMAGE','DICVP','PH4H'), 'Invalid domain: '+domain
+        assert domain in ('DCC','IPS-PILGRIMAGE','DICVP','PH4H'), 'Invalid domain: '+domain
 
-            assert ('TLS', 'TLS.pem') in ofiles[domain], f'TLS cert is missing in domain {domain}'
-            assert ('TLS', 'CA.pem') in ofiles[domain], f'TLS/CA cert is missing in domain {domain}'
-            assert ('UP', 'UP.pem') in ofiles[domain], f'UP cert is missing in domain {domain}'
-            # assert ('SCA', 'SCA.pem') in ofiles[domain], f'SCA cert is missing in domain {domain}' 
+        assert ('TLS', 'TLS.pem') in ofiles[domain], f'TLS cert is missing in domain {domain}'
+        assert ('TLS', 'CA.pem') in ofiles[domain], f'TLS/CA cert is missing in domain {domain}'
+        assert ('UP', 'UP.pem') in ofiles[domain], f'UP cert is missing in domain {domain}'
+        # assert ('SCA', 'SCA.pem') in ofiles[domain], f'SCA cert is missing in domain {domain}'
